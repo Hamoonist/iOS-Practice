be02c0b1cb60c8545917ede481b030a9900523d2
hamoon jamshidi meydandar <jamshidimeydandar@gmail.com>
2020-08-19 15:48:42 +0200

    Add border width to UIView, accessible from Storyboards

---
+++ iOS-Practice/GUIUtilities/UIViewBorderExtensions.swift
@@ -0,0 +1,40 @@
+//
+//  UIViewBorderExtensions.swift
+//  iOS-Practice
+//
+//  Created by Hamoon Jamshidi Meydandar on 2020. 08. 19..
+//
+
+import UIKit
+
+@IBDesignable extension UIView {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
