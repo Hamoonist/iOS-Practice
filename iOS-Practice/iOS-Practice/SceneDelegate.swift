14b7d1c6d494777325e4cb0a3199cc81b5aa6698
hamoon jamshidi meydandar <jamshidimeydandar@gmail.com>
2020-09-07 23:16:28 +0200

    Fix details on Shopping.storyboard

--- iOS-Practice/iOS-Practice/SceneDelegate.swift
+++ iOS-Practice/iOS-Practice/SceneDelegate.swift
@@ -12,13 +12,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
-        guard let controller = UIStoryboard(name: "Instagram", bundle: nil)
-        .instantiateInitialViewController() else { return }
-        window.rootViewController = controller
-        self.window = window
-        window.makeKeyAndVisible()
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        let window = UIWindow(windowScene: windowScene)
+//        guard let controller = UIStoryboard(name: "Instagram", bundle: nil)
+//        .instantiateInitialViewController() else { return }
+//        window.rootViewController = controller
+//        self.window = window
+//        window.makeKeyAndVisible()
     }
 }
 
